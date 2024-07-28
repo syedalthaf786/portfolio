314385652d20e5da70ea06b68787ad7372498644
syedalthaf786 <142567097+syedalthaf786@users.noreply.github.com>
2024-07-29 02:58:00 +0530

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -24,14 +24,20 @@
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
+  
+  // $contact->smtp = array(
+  //   'host' => 'example.com',
+  //   'username' => 'example',
+  //   'password' => 'pass',
+  //   'port' => '587'
+  // );
+$contact->smtp = array(
+  'host' => 'smtp.gmail.com',
+  'username' => 'syedalthaf760@gmail.com',
+  'password' => 'Affu@786',
+  'port' => '587'
+);
+  
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
